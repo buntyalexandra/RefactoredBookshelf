2460e34b33f4ab7f25c48718138093b84f0f16b5
Your Name <emilybuetow@Johns-MacBook-Pro-3.local>
2023-01-06 15:18:30 -0500

    functional code for everything leading up to favorite feature. Still need to create a reduce function to count the total of fave books.

--- js/refactoredIndex.js
+++ js/refactoredIndex.js
@@ -1,14 +1,26 @@
 /*
-Render a book card for each book in bookData
-add the books to the bookshelf
+- Render a book card for each book in bookData
+- add the books to the bookshelf
+- Favorite button for each book, 
 */
 
 const bookList = bookData; 
-const bookshelfSection = document.querySelector(".bookshelf");
+const bookshelfSection = document.querySelector(".bookshelfSection");
+const favoriteBooks = [];
+let favoriteBooksCount = 0;
+
+const labelAsFavorite = (book) => {
+    favoriteBooks.push(book);
+    favoriteBooksCount++;
+    console.log(favoriteBooks);
+    console.log(favoriteBooksCount);
+    renderAllBooks(bookList);
+}
 
 const renderBook = (book) => {
     const {author, language, subject, title} = book; 
     const bookCard = document.createElement("section");
+    bookCard.className = "card"; 
     const bookTitle = document.createElement("h1");
     bookTitle.className = "titleCard";
     bookTitle.textContent = title; 
@@ -22,6 +34,16 @@ const renderBook = (book) => {
     bookSubjectTags.className = "subjectTags";
     bookSubjectTags.textContent = `Subject tags include: ${subject}` 
     bookCard.append(bookTitle, bookAuthor, bookLanguage, bookSubjectTags);
+    // Favorite switch
+    const bookFavorited = document.createElement("h1");
+    bookFavorited.className = "favoritedBook";
+    bookFavorited.textContent = "FAVE";
+    const favoriteButton = document.createElement("button");
+    favoriteButton.className = "favoriteButton"; 
+    favoriteButton.textContent = "Favorite this book";
+    favoriteButton.addEventListener("click", () => labelAsFavorite(book));
+    const elementToAdd = (favoriteBooks.includes(book)) ? bookFavorited : favoriteButton;
+    bookCard.append(elementToAdd);
     return bookCard;
 }
 
